c6e5e4637ed51218d6d42f1844ab17af3687424e
poornima-sivanand <poornima.sivanand@gov.bc.ca>
2022-04-13 11:30:52 -0700

    fix

--- app/server/index.js
+++ app/server/index.js
@@ -41,7 +41,7 @@
   app.use(express.json())
  
 
-  var url = 'mongodb://127.0.0.1:27017/TheNeighborhood';
+  var url = 'mongodb://userMQG:s2SdgYOaaLRCfsjb@mongodb:27017/TheNeighborhood';
  
   // if OPENSHIFT env variables are present, use the available connection info:
   if (process.env.MONGODB_URL) {
